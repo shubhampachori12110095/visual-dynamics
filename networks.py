069194fe975bdcd6c5fa75b71a40e64c7cd11694
Zhijian Liu <zhijianliu.cs@gmail.com>
2018-01-01 17:59:58 -0500

    network structure

---
+++ networks.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import torch.nn as nn
+
+
+class MotionEncoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, inputs):
+        pass
+
+
+class KernelDecoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, inputs):
+        pass
+
+
+class ImageEncoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, inputs):
+        pass
+
+
+class MotionDecoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, inputs):
+        pass
