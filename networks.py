9e7dae8fbae7d999a6c0a5afdf5fae37f6440213
Zhijian Liu <zhijianliu.cs@gmail.com>
2018-01-02 05:39:51 -0500

    kernel decoder

--- networks.py
+++ networks.py
@@ -61,22 +61,42 @@ class MotionEncoder(nn.Module):
 
 
 class KernelDecoder(nn.Module):
-    def __init__(self, num_scales, in_channels, out_channels, kernel_size):
+    def __init__(self, num_scales, num_groups, in_channels, out_channels, kernel_size):
         super(KernelDecoder, self).__init__()
-        pass
+
+        # settings
+        self.num_scales = num_scales
+        self.num_groups = num_groups
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+
+        # channels
+        self.num_channels = num_scales * num_groups * in_channels * out_channels
+
+        # decoder
+        self.decoder = nn.Sequential(
+            ConvPool2D(channels = [self.num_channels] * 3, kernel_sizes = 5,
+                       batch_norm = True, nonlinear_type = 'RELU', last_nonlinear = False,
+                       sampling_type = 'NONE', sampling_sizes = 1),
+            nn.BatchNorm2d(self.num_channels)
+        )
+        self.decoder.apply(weights_init)
 
     def forward(self, inputs):
-        pass
+        inputs = inputs.view(-1, self.num_channels, self.kernel_size, self.kernel_size)
+        outputs = self.decoder.forward(inputs)
+        outputs = outputs.view(-1, self.num_scales, self.num_groups, self.in_channels, self.out_channels,
+                               self.kernel_size, self.kernel_size)
+        return outputs
 
 
 class MotionDecoder(nn.Module):
     def __init__(self):
         super(MotionDecoder, self).__init__()
-        pass
 
     def forward(self, inputs):
         pass
-
 
 class VDNet(nn.Module):
     def __init__(self, scales = [.25, .5, 1, 2]):
@@ -96,28 +116,40 @@ class VDNet(nn.Module):
                                             kernal_sizes = 5,
                                             sampling_sizes = [4, 1, 2, 1, 2, 1])
 
-        # self.kernel_decoder = KernelDecoder()
+        # kernel decoder
+        self.kernel_decoder = KernelDecoder(num_scales = len(scales),
+                                            num_groups = 32,
+                                            in_channels = 1,
+                                            out_channels = 1,
+                                            kernel_size = 5)
+
         # self.motion_decoder = MotionDecoder()
 
     def forward(self, inputs, sampling_type = 'NONE'):
         # sanity check
+        assert sampling_type in ['NONE'], 'unsupported sampling type "{0}"'.format(sampling_type)
 
+        #
         if sampling_type == 'NONE':
             i_inputs, m_inputs = inputs
         else:
             i_inputs = inputs
 
+        # image encoder
         f = self.image_encoder.forward(i_inputs)
-        print(f)
 
+        # motion encoder
         if sampling_type == 'NONE':
             z, params = self.motion_encoder.forward(m_inputs)
         else:
             assert False
 
-        # k = self.kernel_decoder.forward(z)
-        #
-        #
+        # kernel decoder
+        k = self.kernel_decoder.forward(z)
+        print(k.size())
+
+        # cross convolution
+
         # m = F.conv(f, k)
         #
         # outputs = self.motion_decoder.forward(m)
